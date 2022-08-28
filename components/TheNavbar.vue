7a65043eedbf0ac1f1901483768f7990f75d37b8
@Aqib-cyber2 <aqibafridi0345@gmail.com>
2022-08-29 03:05:05 +0500

    nuxt recipe app components with styling

---
+++ components/TheNavbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="mb-6">
+        <nav class="mx-auto py-8 flex flex-wrap item-center">
+     
+            <div class="brand">
+                <img class="w-40" src="https://fabrx.co/preview/tastebite/assets/images/brands/brand4.svg" alt="logo">
+            </div>
+
+            <ul class="
+                pt-4
+                text-base text-gray-700
+                md:flex
+                md:justify-between 
+                md:pt-0
+                ml-auto"
+            >
+                <li>
+                    <a class="md:p-4 py-2 block hover:text-purple-400" href="#">Home</a>
+                </li>
+                <li>
+                    <a class="md:p-4 py-2 block hover:text-purple-400" href="#">About</a>
+                </li>
+            </ul>
+       
+            
+        </nav>
+    </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="container mx-auto bg-gray-300 p-8">
-    <h1 class="font-bold text-gray-600 text-lg">Hello, Tailwind 3!</h1>
+  <div class="container mx-auto px-8 font-sans">
+    <TheNavbar />
+
+    <!-- slider section -->
+    <section>
+      <div class="flex bg-sky-200 items-center">
+
+        <!-- left column -->
+        <div class="w-7/12">
+          <img class="w-full" src="@/assets/imgs/2.jpg" alt="noodle-slider-img">
+        </div>
+        <!-- right column -->
+        <div class="w-5/12 px-12">
+          <div class="flex">
+            <svg class="mr-4" data-name="feather-icon/trending-up" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+              viewBox="0 0 20 20">
+              <rect data-name="Bounding Box" width="20" height="20" fill="rgba(255,255,255,0)"></rect>
+              <path
+                d="M.244,11.423a.834.834,0,0,1,0-1.178L6.494,3.994a.834.834,0,0,1,1.178,0L11.25,7.571l5.9-5.9H14.167a.833.833,0,1,1,0-1.667h5A.833.833,0,0,1,20,.833v5a.834.834,0,0,1-1.667,0V2.845L11.839,9.339a.834.834,0,0,1-1.179,0L7.083,5.761l-5.66,5.661a.834.834,0,0,1-1.179,0Z"
+                transform="translate(0 4.167)" fill="#ff642f"></path>
+            </svg>
+            <span class="text-sm font-medium">85% would make this again</span>
+          </div>
+
+          <h1 class="text-5xl font-bold my-5 leading-tight">Mighty Super Cheesecake</h1>
+
+          <p class="text-lg">
+            Look no further for a creamy and ultra smooth
+            classic cheesecake recipe! no one can deny its simple decadence.
+          </p>
+        </div>
+
+      </div>
+    </section>
   </div>
-</template> 
+</template>
