5d0449bd2e66a1b2294199f4bd4f413daf85dcbe
Muhammad Aqib <aqibafridi0345@gmail.com>
2022-09-02 21:13:04 +0500

    updated Nuxt app with external css

---
+++ components - Copy/TheNavbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="">
+        <nav class="mx-auto border border-transparent py-4 flex flex-wrap item-center">
+     
+            <div class="brand">
+                <img class="w-36" src="https://fabrx.co/preview/tastebite/assets/images/brands/brand4.svg" alt="logo">
+            </div>
+
+            <ul class="
+                pt-4
+                text-base text-gray-700
+                md:flex
+                md:justify-between 
+                md:pt-0
+                ml-auto"
+            >
+                <li>
+                    <a class="md:p-4 py-2 block hover:text-purple-400" href="#">Home</a>
+                </li>
+                <li>
+                    <a class="md:p-4 py-2 block hover:text-purple-400" href="#">About</a>
+                </li>
+            </ul>
+       
+            
+        </nav>
+    </header>
+</template>
