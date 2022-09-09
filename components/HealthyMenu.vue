671aed4874d8d1fd9d41b3c1d155218a06d8b10e
Muhammad Aqib <aqibafridi0345@gmail.com>
2022-09-09 18:24:00 +0500

    Tabs cards

--- components/HealthyMenu.vue
+++ components/HealthyMenu.vue
@@ -41,7 +41,7 @@
 <template>
     <section class="mb-10">
         <!-- title -->
-        <div class="px-3 mb-8 w-5/12">
+        <div class="px-3 mb-10 w-5/12">
 
             <h1 class="title font-Alovera font-bold leading-none flex items-start justify-between text-indigo-400 mb-5">
                 <span class="
@@ -91,6 +91,43 @@
                     {{recipe.title}}
 
                 </button>
+            </div>
+
+            <div class="w-8/12 tab-content">
+                <div class="flex px-8 gap-8">
+                    <div class="card bg-white p-6 w-6/12">
+                        <div class="card-img">
+                            <img class="w-full" src="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="pizza img">
+                        </div>
+                        <div class="card-body text-center py-5">
+                            <p class="text-indigo-400 text-base mb-5">
+                                Lorem, ipsum dolor sit amet consectetur.
+                            </p>
+                            
+                            <a href="#" 
+                            class="px-5 inline-block py-3 text-white bg-yellow
+                            text-sm tracking-[.2rem] rounded-2xl border border-white 
+                            font-bold
+                            "> See Detail </a>
+                        </div>
+                    </div>
+                    <div class="card p-6 w-6/12 border-8 border-white">
+                        <div class="card-img">
+                            <img class="w-full" src="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="pizza img">
+                        </div>
+                        <div class="card-body text-center py-5">
+                            <p class="text-indigo-400 text-base mb-5">
+                                Lorem, ipsum dolor sit amet consectetur.
+                            </p>
+                            
+                            <a href="#" 
+                            class="px-5 inline-block py-3 text-indigo-400 bg-white
+                            text-sm tracking-[.2rem] rounded-2xl border border-white 
+                            font-bold
+                            "> See Detail </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
